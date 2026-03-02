442cd5c40422c1aecf1a95e1693621fe141583fb
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-03-02 21:12:08 +0200

    clear

--- Shop.Domain/Entities/Categories.cs
+++
@@ -1,9 +0,0 @@
-namespace Shop.Domain.Entities;
-
-public class Categories
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-    public ICollection<Product> Products { get; set; } = new List<Product>();
-}
